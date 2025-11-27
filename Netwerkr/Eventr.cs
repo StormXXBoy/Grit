067f3effa4e9d39964459c29d69da359839ba94f
storm <storm@stormxxboy.com>
2025-11-27 17:27:30 +0100

    netwerrk

---
+++ Netwerkr/Eventr.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netwerkr
+{
+    public class Eventr
+    {
+        public string name { get; }
+        public event Action<string> received;
+
+        public Eventr(string Name)
+        {
+            name = Name;
+        }
+
+        internal void Invoke(string data)
+        {
+            received?.Invoke(data);
+        }
+    }
+}
---
+++ Netwerkr/Netwerkr.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Netwerkr
+{
+    public static class Netwerkr
+    {
+        public static async Task<NetwerkrConnection> connect(string ip, int port = 3000)
+        {
+            var client = new TcpClient();
+            await client.ConnectAsync(ip, port);
+            return new NetwerkrConnection(client);
+        }
+    }
+
+}
